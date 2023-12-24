04c44515b8b2e4508c0e8177bae7faa5ea1226dc
IbrokhimShukurullayev <ibrokhimjonshukurullayev>
2023-12-25 00:50:19 +0500

    ibrohim

---
+++ js/main.js
@@ -0,0 +1,75 @@
+// String1
+
+// let n = 10;
+
+// for (let i = 65; i < 65 + n; i++) {
+//     let stringBig = String.fromCharCode(i);
+//     console.log(stringBig);
+// }
+
+// String2
+
+// let n = "1111";
+
+// if (/[0-9]/.test(n)) {
+//   console.log("digit");
+// } else if (/[a-zA-Z]/.test(n)) {
+//   console.log("lotin");
+// } else {
+//   console.log("nol");
+// }
+
+// String3
+
+// let satr = "abdulaziz";
+
+// let first = satr.charCodeAt(0);
+
+// let last = satr.charCodeAt(satr.length - 1);
+
+// console.log("Birinchi belgi UTF-16 kodi:", first);
+// console.log("Oxirgi belgi UTF-16 kodi:", last);
+
+// String4
+
+// let n = 3;
+// let belgi = "ROLEX ";
+// let sum = belgi;
+
+// for (let i = 1; i < n; i++) {
+//   sum += belgi;
+// }
+
+// console.log(sum);
+
+// String5
+
+// let satr = "Abdulaziz";
+
+// let res = "";
+
+// for (let i = satr.length - 1; i >= 0; i--) {
+//   res += satr[i];
+// }
+
+// console.log(res);
+
+// String6
+
+// function getStringStars(str, N) {
+//   let res = "";
+//   for (let i = 0; i < str.length; i++) {
+//     res += str[i];
+//     for (let j = 1; j <= N; j++) {
+//       res += "*";
+//     }
+//   }
+//   return res;
+// }
+
+// let satr = "Abdulaziz";
+// let N = 5;
+
+// let = newSatr = getStringStars(satr, N);
+
+// console.log(newSatr);
